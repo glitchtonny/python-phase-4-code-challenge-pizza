2de9a4d572e2e1954575c9903e5a63ebbde06ac7
g;itchtonny <sirmwangitosh@gmail.com>
2024-07-28 13:05:16 +0300

    complete pizza challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,66 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    restaurants_list = [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants]
+
+    response = make_response(jsonify(restaurants_list), 200)
+    return response
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        response = make_response(jsonify(restaurant.to_dict()), 200)
+    else:
+        response = make_response(jsonify({'error': 'Restaurant not found'}), 404)
+    return response
+
+@app.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if restaurant:
+        db.session.delete(restaurant)
+        db.session.commit()
+        response = make_response(jsonify({'message': 'Restaurant deleted successfully'}), 200)
+    else:
+        response = make_response(jsonify({'error': 'Restaurant not found'}), 404)
+    return response
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    pizzas_list = [
+       { 
+            'id': pizza.id,
+            'name': pizza.name,
+            'ingredients': pizza.ingredients,
+
+        }
+        for pizza in pizzas
+    ]
+    response = make_response(jsonify(pizzas_list), 200)
+    return response
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    try:
+        new_restaurant_pizza = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        db.session.add(new_restaurant_pizza)
+        db.session.commit()
+        response = make_response(jsonify(new_restaurant_pizza.to_dict()), 201)
+    except ValueError as e:
+        response = make_response(jsonify({"errors": [str(e)]}), 400)
+    except Exception as e:
+        response = make_response(jsonify({"errors": ["An error occurred: " + str(e)]}), 500)
+    return response
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
